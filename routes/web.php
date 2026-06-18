9ab015e15b3ce1ca190d7e2bc5af6ea87171635e
Siti Masito <sitimasito@saintek.unipdu.ac.id>
2026-06-19 01:16:46 +0700

    fix model relation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [
+    DashboardController::class,
+    'index'
+]);
+
+Route::post('/transaksi', [
+    DashboardController::class,
+    'store'
+]);
+Route::get('/test', function () {
+    return App\Models\Layanan::count();
 });
